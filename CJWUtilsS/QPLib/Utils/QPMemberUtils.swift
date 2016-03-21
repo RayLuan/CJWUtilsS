ee636b06283592fe74e65bb254893e94d38d357f
Ray <379370974@qq.com>
2016-03-21 22:45:57 +0800

    update

--- CJWUtilsS/QPLib/Utils/QPMemberUtils.swift
+++ CJWUtilsS/QPLib/Utils/QPMemberUtils.swift
@@ -14,9 +14,9 @@ private let KEY_PASSWORD = "PASSWORD"
 private let KEY_YES = "YES"
 private let KEY_NO = "NO"
 
-class QPMemberUtils: NSObject {
+public class QPMemberUtils: NSObject {
     
-    var memberInfo = NSDictionary() {
+    public var memberInfo = NSDictionary() {
         didSet {
             if let imToken = memberInfo["imToken"] as? String {
                 self.imToken = imToken
@@ -24,14 +24,14 @@ class QPMemberUtils: NSObject {
         }
     }
     
-    var loginFlag = false
+    public var loginFlag = false
     
-    class func login(){
+    public class func login(){
         QPMemberUtils.sharedInstance.loginFlag = true
     }
     
-    var imToken = ""
-    class func logout(){
+    public var imToken = ""
+    public class func logout(){
 //        QPHttpUtils.sharedInstance.logout({ (resp) -> Void in
 //            //
 //            }) { () -> Void in
@@ -48,31 +48,31 @@ class QPMemberUtils: NSObject {
     //        return false
     //    }
     
-    class func clearUserInfo(){
+    public class func clearUserInfo(){
         QPKeyChainUtils.removeKey(KEY_PASSWORD)
         QPKeyChainUtils.removeKey(KEY_ACCOUNT)
     }
     
-    class func isLoginInfoExist(){
+    public class func isLoginInfoExist(){
         if QPKeyChainUtils.stringForKey(KEY_ACCOUNT) != nil && QPKeyChainUtils.stringForKey(KEY_PASSWORD) != nil {
         }
     }
     
-    class func saveInfo(account:String,password:String){
+    public class func saveInfo(account:String,password:String){
         QPKeyChainUtils.setString(account, forKey: KEY_ACCOUNT)
         QPKeyChainUtils.setString(password, forKey: KEY_PASSWORD)
         QPMemberUtils.login()
     }
     
-    class func account() -> String? {
+    public class func account() -> String? {
         return QPKeyChainUtils.stringForKey(KEY_ACCOUNT)
     }
     
-    class func password() -> String? {
+    public class func password() -> String? {
         return QPKeyChainUtils.stringForKey(KEY_PASSWORD)
     }
     
-    class var sharedInstance : QPMemberUtils {
+    public class var sharedInstance : QPMemberUtils {
         struct Static {
             static var onceToken : dispatch_once_t = 0
             static var instance : QPMemberUtils? = nil
@@ -83,26 +83,26 @@ class QPMemberUtils: NSObject {
         return Static.instance!
     }
     
-    var memberId : Int {
+    public var memberId : Int {
         if let id = memberInfo["id"] as? Int {
             return id
         }
         return -1
     }
     
-    var name : String {
+    public var name : String {
         if let value = memberInfo["name"] as? String {
             return value
         }
         return ""
     }
     
-    var thumbnail : String {
+    public var thumbnail : String {
         let tmp = logo.stringByReplacingOccurrencesOfString(".png", withString: "_s.png")
         return tmp
     }
     
-    var logo : String {
+    public var logo : String {
         if let value = memberInfo["logo"] as? String {
             return value
         }
